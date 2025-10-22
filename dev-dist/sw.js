010025502b681098de0ee90353926f542a3bbe70
anandu112 <andu0870@uni.sydney.edu.au>
2025-10-23 06:28:31 +1100

    Twenty-eighth

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f8c404e3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rodl3gl641o"
+    "revision": "0.ph85gf9sruo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
