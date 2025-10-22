780c07b7845b11975b583a0f3f089fbcc92fd96a
anandu112 <andu0870@uni.sydney.edu.au>
2025-10-23 05:15:19 +1100

    Seventeen

--- src/components/VoiceOrb.tsx
+++ src/components/VoiceOrb.tsx
@@ -64,11 +64,6 @@ const VoiceOrb: React.FC<VoiceOrbProps> = ({ state, audioLevel = 0, isSpeaking =
         {/* 发光层 */}
           <div className="orb-glow" />
           
-          {/* 眼睛 */}
-          <div className="orb-eyes">
-            <div className="orb-eye orb-eye-left" />
-            <div className="orb-eye orb-eye-right" />
-          </div>
         </div>
       </div>
     </div>
