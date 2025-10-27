a4134fccf1f36fc5d6e527a40133ae133d45a2a3
anandu112 <andu0870@uni.sydney.edu.au>
2025-10-27 21:27:28 +1100

    Thirty-third

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f8c404e3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.31me10u3pl"
+    "revision": "0.fk6mhqcn9jo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
